bb0df03db0a75ede5d5a2018026588689232a45b
Rastislav Hepner <rastislav.hepner@gmail.com>
2018-08-14 17:49:28 +0200

    Check if filename exists

--- droplist.py
+++ droplist.py
@@ -6,7 +6,17 @@ import os.path
 def usage():
   print("Usage: droplist {filepath}")
 
+def error1():
+  print("Error: file \'{}\' doesn't exist".format(filename)) 
+
 if len(sys.argv) == 1:
+  usage()
+  sys.exit(1)
+
+filename = sys.argv[1]
+
+if not os.path.exists(filename):
+  error1()
   usage()
   sys.exit(1)
 
