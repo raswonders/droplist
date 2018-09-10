b6d1f602c0316f86f18e9221cae9406fe06e8203
Rastislav Hepner <rastislav.hepner@gmail.com>
2018-09-10 11:05:02 +0200

    Sum of all drops added

--- droplist.py
+++ droplist.py
@@ -3,11 +3,14 @@
 import sys
 import os.path
 
+from collections import OrderedDict
+
 def usage():
   print("Usage: droplist {filepath}")
 
 def error1():
   print("Error: file \'{}\' doesn't exist".format(filename)) 
+
 
 if len(sys.argv) == 1:
   usage()
@@ -22,13 +25,24 @@ if not os.path.exists(filename):
 
 fh = open(filename, "r")
 lines = fh.readlines()
-func_stats = {}
+func_stats = {'TOTAL' : 0}
 
 for x in lines:
   func = ' '.join(x.split()[3:])
   drops = int(x.split()[0])  
+  func_stats['TOTAL'] += drops
   if func in func_stats:
     func_stats[func] += drops
   else:
     func_stats[func] = drops
+
+func_stats_sorted = OrderedDict(sorted(func_stats.items(), key=lambda t: t[1], reverse=True))
+
+print("{:^15s}|{:^65}".format("Drops", "Function"))
+print("-" * 80)
+for k, v in func_stats_sorted.items():
+  print("{:<15d}{:65s}".format(v, k))
+
+
+
 
