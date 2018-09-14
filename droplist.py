51ccc209fecca3639a37b98d5934cbf6028c9c1a
Rastislav Hepner <rastislav.hepner@gmail.com>
2018-09-14 17:37:59 +0200

    parse_drops() modified, returns drops, precision)

--- droplist.py
+++ droplist.py
@@ -13,7 +13,7 @@ def open_file(parser, fn):
   except FileNotFoundError:
     parser.error("File \'{}\' doesn't exist".format(fn))
 
-def parse_drops(parser, p):
+def parse_drops(parser, p, p_def):
   pattern = re.compile(r'(\d+)(:(\d+)(%)?)?$')    
   m = pattern.match(p)
   if m == None:
@@ -21,29 +21,26 @@ def parse_drops(parser, p):
     parser.error("Invalid value for drops")
   elif m.group(2) == None:
     # drop count without precision 
-    return m.group(1)
+    precision = int(round(float(p_def) * float(m.group(1)) / 100))
   elif m.group(4) == '%':
     # drop count with precision as percentage
     if int(m.group(3)) > 100:
       parser.error("Precision cannot be higher than 100%")
-    precision = round(int(m.group(3)) * int(m.group(1))  / 100) 
-    # drop count with precision as whole number 
+    precision = int(round(float(m.group(3)) * float(m.group(1))  / 100)) 
   else:
+    # drop count with precision as whole number 
     precision = int(m.group(3))
 
   drops = int(m.group(1))
-  rside = drops + precision
-  lside = drops - precision
-  if lside < 0:
-    lside = 0
-  return (lside, rside)
+  return (drops, precision)
 
-
+# Default precision in percentage  
+p_def = 2
 parser = argparse.ArgumentParser()
 parser.add_argument("filename", help="dropwatch output file",
                     type=lambda x: open_file(parser, x))
 parser.add_argument("-d", "--drops", help="[d[:p]] d number of drops you're searching for, p is allowed deviation from drop count. Deviation can be expressed either in percentage or as a whole number",
-                    type=lambda x: parse_drops(parser, x))
+                    type=lambda x: parse_drops(parser, x, p_def))
 args = parser.parse_args()
 
 fh = args.filename
@@ -67,3 +64,9 @@ for k, v in func_stats_sorted.items():
   print("{:<15d}{:65s}".format(v, k))
 
 print(args.drops)
+"""
+if args.drops != None:
+  drops = args.drops[0]
+  precision = args.drops[1]
+  func_precise = [ k for k,v in func_stats_sorted.items() if v == drops ]
+"""
