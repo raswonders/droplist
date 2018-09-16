724fd5a98436f4710fab81675361ef5a682aa179
Rastislav Hepner <rastislav.hepner@gmail.com>
2018-09-16 22:28:15 +0200

    find_func() implemented

--- droplist.py
+++ droplist.py
@@ -33,9 +33,6 @@ def list_func_comb(iterator, f_dict, total):
       print("{:<15d}{:65s}".format(f_dict[func], func))
     print("{:<15d}{:65s}".format(total, "TOTAL"))
 
-    
-
-
 def open_file(parser, fn):
   try:
     return open(fn, "r")
@@ -68,28 +65,66 @@ def compute_drops(iterator, f_dict):
     drops += f_dict[i]
   return drops
 
-def find_func(drops, precision, sorted_dict):
-  rside = drops + precision
-  lside = drops - precision 
-  if lside < 0:
-    lside = 0
-  d = OrderedDict()
-  for key,val in sorted_dict.items():
-    if val <= rside:
-      d[key] = val
-  #for k in range(1, len(d) + 1):
-  for k in range(1, 3):
-    l_combinations = list(itertools.combinations(d.keys(), k)) 
-    d_combinations = {key: compute_drops(key, d) for key in l_combinations}    
-    for k,v in d_combinations.items():
-      if lside <= v <= rside:
-        list_func_comb(k, d, v)
-  #list_func(None, d)   
-    
-  
-
-
-
+def find_func(l, r, sorted_dict):
+  step_percent = 5
+  ld = [] 
+  i_k_l_combinations = [] 
+  # How fast interval grows
+  step = (float(l) * step_percent) / 100
+  within_range = False
+  jump = 0
+  autosearch = False
+  for i in range(21):
+    if i == 1:
+      if r == l and not within_range:
+        print("No function or combination of them with drop count {} has been found.".format(drops)) 
+        ans = input("Do you want to continue with autosearch? [Y]/N: ")
+        if ans == 'y' or ans == 'Y' or not ans:
+          autosearch = True
+        else:
+          break
+      elif r == l and within_range:
+        break
+    if autosearch and within_range:
+      break
+    i_k_l_combinations.append([])
+    rside = r + round(i * step) 
+    lside = l - round(i * step)
+    if lside < 0:
+      lside = 0
+    d = OrderedDict()
+    # Build dictionary with functions which value <= right side of interval 
+    for key,val in sorted_dict.items():
+      if val <= rside:
+        d[key] = val
+    ld.append(d)
+    if i > 0:
+      # Build dictionary with just functions which were newly added this round
+      d = { k : d[k] for k in set(d) - set(ld[i-1])}  
+      new = len(d.keys())
+      if not d:
+        # if there are no newly added functions remember iteration i and repeat  
+        if not jump:
+          jump = i
+        continue
+    # If we skipped some iterations of i without saving combinations, correct references 
+    if jump:
+      i_correct = jump
+      jump = 0
+    else:
+      i_correct = i
+    for k in range(1, len(ld[i].keys()) + 1):
+      l_combinations = list(itertools.combinations(ld[i].keys(), k)) 
+      i_k_l_combinations[i].append(l_combinations)
+      if i > 0:
+        # Optimization skip combinations which were already computed  
+        if k <= len(ld[i].keys()) - new:
+          l_combinations = [ x for x in set(l_combinations) - set(i_k_l_combinations[i_correct-1][k-1])] 
+      d_combinations = {key: compute_drops(key, ld[i]) for key in l_combinations}    
+      for k,v in d_combinations.items():
+        if lside <= v <= rside:
+          list_func_comb(k, ld[i], v)
+          within_range = True
 
 # Parse arguments
 parser = argparse.ArgumentParser()
@@ -116,6 +151,10 @@ func_stats_sorted = OrderedDict(sorted(func_stats.items(), key=lambda t: t[1], r
 if args.drops != None:
   drops = args.drops[0]
   precision = args.drops[1]
-  find_func(drops, precision, func_stats_sorted)
+  rside = drops + precision
+  lside = drops - precision 
+  if lside < 0:
+    lside = 0
+  find_func(lside, rside, func_stats_sorted)
 
 
