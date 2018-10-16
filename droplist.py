f09661c3a0c54f9947576605b9cb652610727928
Rastislav Hepner <rastislav.hepner@gmail.com>
2018-10-16 13:19:02 +0200

    Major rework

--- droplist.py
+++ droplist.py
@@ -3,8 +3,12 @@
 import sys
 import os.path
 import argparse
+import re
 
 from collections import OrderedDict
+
+class TimeSignature(Exception):
+  pass
 
 def list_func(f_dict):
   print("{:^15s}|{:^65}".format("Drops", "Function"))
@@ -18,30 +22,60 @@ def open_file(parser, fn):
   except FileNotFoundError:
     parser.error("File \'{}\' doesn't exist".format(fn))
 
-def compute_drops(iterator, f_dict):
-  drops = 0
-  for i in iterator:
-    drops += f_dict[i]
-  return drops
+def time_signature(lines):
+  pattern = re.compile('\d+$')
+  start = pattern.match(lines[0])
+  if start:
+    end = pattern.match(lines[-1])
+    if end:
+      start_time = int(end.group())
+      end_time = int(start.group())
+      elapsed_time = end_time - start_time 
+      if elapsed_time > 0:
+        return (start_time, end_time, elapsed_time)
+  raise TimeSignature 
+
+def drop_stats(lines):
+  func_stats = {'ALL' : 0}
+  """
+  match groups of interest from pattern
+  group(1) drops e.g. "233"
+  group(3) function name with byte offset e.g. "skb_release_data+9a"
+  group(4) byte offset for instance e.g. "+9e"
+  group(5) memory address of a function e.g. "0xffffffff819ac3ca"
+  """
+  pattern = re.compile('(\d+)( drops at )(\w+(\+\w+)*) \((0x\w+)\)') 
+  for x in lines:
+    m = pattern.match(x)
+    if m:
+      func = m.group(3) 
+      drops = int(m.group(1))  
+      func_stats['ALL'] += drops
+      if func in func_stats:
+        func_stats[func] += drops
+      else:
+        func_stats[func] = drops
+  return OrderedDict(sorted(func_stats.items(), key=lambda t: t[1], reverse=True))
 
 def parse_dropwatch(fh):
   lines = fh.readlines()
-  func_stats = {'TOTAL' : 0}
-  for x in lines:
-    func = ' '.join(x.split()[3:])
-    drops = int(x.split()[0])  
-    func_stats['TOTAL'] += drops
-    if func in func_stats:
-      func_stats[func] += drops
-    else:
-      func_stats[func] = drops
-  return OrderedDict(sorted(func_stats.items(), key=lambda t: t[1], reverse=True))
+  try:
+    time_sign = time_signature(lines)
+  except TimeSignature:
+    # without time info
+    return drop_stats(lines)
+  # with time info
+  return drop_stats(lines, time_sign)
 
-# Parse arguments
-parser = argparse.ArgumentParser()
-parser.add_argument("filename", help="dropwatch output file",
-                    type=lambda x: open_file(parser, x))
-args = parser.parse_args()
-func_stats_sorted = parse_dropwatch(args.filename) 
-list_func(func_stats_sorted)
+def main():
+  # Parse arguments
+  parser = argparse.ArgumentParser()
+  parser.add_argument("filename", help="dropwatch output file",
+                      type=lambda x: open_file(parser, x))
+  args = parser.parse_args()
+  list_func(parse_dropwatch(args.filename))
+
+if __name__ == "__main__":
+  main()
+
 
